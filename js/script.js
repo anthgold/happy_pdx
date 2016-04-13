500f0d25f9cdb02b94633ff0fde6d0371acbde21
Anthony Gold <anthgold@gmail.com>
2016-04-13 15:10:50 -0700

    creates bar constructor and five dummy bar objects

--- js/script.js
+++ js/script.js
@@ -1,90 +1,24 @@
+// Business Logic
+  // Bar Object
+function Bar(name, address, website, phone) {
+  this.name = name;
+  this.address = address;
+  this.website = website;
+  this.phone = phone;
+}
+var groundKontrolBar = new Bar ("Ground Kontrol Classic Arcade", '511 NW Couch St, Portland, OR 97209', 'http://groundkontrol.com/', '(503) 796-9364');
+var baileysTaproomBar = new Bar ("Bailey's Taproom", '213 SW Broadway, Portland, OR 97205', 'http://www.baileystaproom.com/', '(503) 295-1004');
+var kentonClubBar = new Bar ("Kenton Club", '2025 N Kilpatrick St, Portland, OR 97217', 'http://www.kentonclub.com/', '(503) 285-3718');
+var valentinesBar = new Bar ("Valentine's", '232 SW Ankeny St, Portland, OR 97204', 'http://www.valentinespdx.com/', '(503) 248-1600');
+var slowBarBar = new Bar ("Slow Bar", '533 SE Grand Ave, Portland, OR 97214', 'http://slowbar.net/', '(503) 230-7767');
 
+
+// UI Logic
 $(document).ready(function() {
-
-// var stickyOffset = $('.sticky').offset().top;
-
-// $(window).scroll(function(){
-//   var sticky = $('.sticky'),
-//       scroll = $(window).scrollTop();
-
-//   if (scroll >= stickyOffset) {
-//     sticky.addClass('fixed');
-//   } else {
-//     sticky.removeClass('fixed');
-//   }
-// });
-
- // This example adds a search box to a map, using the Google Place Autocomplete
-      // feature. People can enter geographical searches. The search box will return a
-      // pick list containing a mix of places and predicted search terms.
-
-      // This example requires the Places library. Include the libraries=places
-      // parameter when you first load the API. For example:
-      // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-
-      function initAutocomplete() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -33.8688, lng: 151.2195},
-          zoom: 13,
-          mapTypeId: google.maps.MapTypeId.ROADMAP
-        });
-
-        // Create the search box and link it to the UI element.
-        var input = document.getElementById('pac-input');
-        var searchBox = new google.maps.places.SearchBox(input);
-        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-        // Bias the SearchBox results towards current map's viewport.
-        map.addListener('bounds_changed', function() {
-          searchBox.setBounds(map.getBounds());
-        });
-
-        var markers = [];
-        // Listen for the event fired when the user selects a prediction and retrieve
-        // more details for that place.
-        searchBox.addListener('places_changed', function() {
-          var places = searchBox.getPlaces();
-
-          if (places.length == 0) {
-            return;
-          }
-
-          // Clear out the old markers.
-          markers.forEach(function(marker) {
-            marker.setMap(null);
-          });
-          markers = [];
-
-          // For each place, get the icon, name and location.
-          var bounds = new google.maps.LatLngBounds();
-          places.forEach(function(place) {
-            var icon = {
-              url: place.icon,
-              size: new google.maps.Size(71, 71),
-              origin: new google.maps.Point(0, 0),
-              anchor: new google.maps.Point(17, 34),
-              scaledSize: new google.maps.Size(25, 25)
-            };
-
-            // Create a marker for each place.
-            markers.push(new google.maps.Marker({
-              map: map,
-              icon: icon,
-              title: place.name,
-              position: place.geometry.location
-            }));
-
-            if (place.geometry.viewport) {
-              // Only geocodes have viewport.
-              bounds.union(place.geometry.viewport);
-            } else {
-              bounds.extend(place.geometry.location);
-            }
-          });
-          map.fitBounds(bounds);
-        });
-      }
+  $("form#choose-pizza").submit(function(event) { // button needs tailoring
+  event.preventDefault();
 
 
 
-});
+  }); // Closes Form-Button Submit
+}); // Closes JQuery
